1fde5fa3aae72748bd7643e3043b68f50532e6ee
CZYBUAA <34918584+CZYBUAA@users.noreply.github.com>
2018-09-10 11:19:45 +0800

    Update dytt_spider.py
    1、修改了关于“有一些页面不被p标签”的问题，解决方案就是getMovieDetails函数里的获取p标签改为只获取到div标签，并输出text
    2、将获取各条详细信息的代码从getContents函数转移到getMovieDetails函数内，getMovieDetails直接返回一个处理结果，避免原来的方案中返回一个原始信息数组并且还需要重新做判空处理
    3、第30、31行增加的一个判断逻辑，是为了处理在获取url时，页面故意提供的一个错误url，造成信息获取错误的问题（反爬）
    4、遗留问题：除了之前说的问题外，还需要处理重新发送请求的问题，程序稳定性不够，通常会因为服务端连接失败造成爬取信息失败并中断程序

--- dytt_spider.py
+++ dytt_spider.py
@@ -10,10 +10,13 @@ from lxml import etree
 #获取对应url的响应页面信息，并返回起HTML文本
 def getText(url):
     header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+        'Referer':'http://www.dytt8.net/',
+        'Connection':'keep-alive',
+        'Accept-Language':'zh - CN, zh;q = 0.8, zh - TW;q = 0.7, zh - HK;q = 0.5, en - US;q = 0.3, en;q = 0.2',
+        'Host':'www.dytt8.net'
     }
-    resp = requests.get(url,headers=header,timeout=50000)
-    print(resp.encoding)                                                    #这里输出的响应页面的编码格式是ISO-8859-1
+    resp = requests.get(url,headers=header,timeout=50000)                                                 #这里输出的响应页面的编码格式是ISO-8859-1
     text = resp.content.decode(encoding="gbk", errors="ignore")             #但是这里只有用gbk解码，控制台输出才不会乱码，反而用ISO-8859-1解码就会乱码
     return text
 
@@ -24,7 +27,10 @@ def getDetailUrl(text):
     url_lists=[]
     for item_list in list:
         detail_url = item_list.xpath(".//a[@class='ulink']/@href")[0]
+        if item_list.xpath(".//a[@class='ulink']/text()")[0]=='[综合电影]':
+            detail_url = item_list.xpath(".//a[@class='ulink']/@href")[1]
         url="http://www.dytt8.net"+detail_url
+        print(url)
         url_lists.append(url)
     return url_lists
 
@@ -42,11 +48,37 @@ def getUrls(start,end):
 #获取详情页中，仅包含关键信息的段落
 def getMovieDetails(text):
     information_html=etree.HTML(text)
-    result_list=information_html.xpath("//div[@id='Zoom']//p")
-    result=[]
-    if result_list!=[]:               #有一些详情页故意不用p标签包含，造成该数组为空，于是产生错误中断程序，需要做一些处理，必要时做一些断点记录
+    result_list=information_html.xpath("//div[@id='Zoom']")
+    movie={}
+    if result_list!=[]:               #判空
         result=result_list[0]
-    return result
+        info = result.xpath(".//text()")
+        for info_item in info:
+            if info_item.startswith("◎译　　名"):
+                info_item = parse_info(info_item, "◎译　　名")
+                movie['translate film name'] = info_item
+            elif info_item.startswith("◎片　　名"):
+                info_item = parse_info(info_item, "◎片　　名")
+                movie['film name'] = info_item
+            elif info_item.startswith("◎年　　代"):
+                info_item = parse_info(info_item, "◎年　　代")
+                movie['year'] = info_item
+            elif info_item.startswith("◎产　　地"):
+                info_item = parse_info(info_item, "◎产　　地")
+                movie['country'] = info_item
+            elif info_item.startswith("◎类　　别"):
+                info_item = parse_info(info_item, "◎类　　别")
+                movie['category'] = info_item
+            elif info_item.startswith("◎豆瓣评分"):
+                info_item = parse_info(info_item, "◎豆瓣评分")
+                movie['douban_rating'] = info_item
+            elif info_item.startswith("◎片　　长"):
+                info_item = parse_info(info_item, "◎片　　长")
+                movie['duration'] = info_item
+            elif info_item.startswith("◎导　　演"):
+                info_item = parse_info(info_item, "◎导　　演")
+                movie['director'] = info_item
+    return movie
 
 def parse_info(info,rule):
     return info.replace(rule,"").strip()
@@ -57,40 +89,13 @@ def getContents(result_urls):
     for item in result_urls:
         detail_url = item
         text = getText(detail_url)
-        detail=getMovieDetails(text)
-        movie = {}
-        if detail!=[]:
-            info=detail.xpath(".//text()")
-            for info_item in info:
-                if info_item.startswith("◎译　　名"):
-                    info_item = parse_info(info_item, "◎译　　名")
-                    movie['translate film name'] = info_item
-                elif info_item.startswith("◎片　　名"):
-                    info_item = parse_info(info_item, "◎片　　名")
-                    movie['film name'] = info_item
-                elif info_item.startswith("◎年　　代"):
-                    info_item = parse_info(info_item, "◎年　　代")
-                    movie['year'] = info_item
-                elif info_item.startswith("◎产　　地"):
-                    info_item = parse_info(info_item, "◎产　　地")
-                    movie['country'] = info_item
-                elif info_item.startswith("◎类　　别"):
-                    info_item = parse_info(info_item, "◎类　　别")
-                    movie['category'] = info_item
-                elif info_item.startswith("◎豆瓣评分"):
-                    info_item = parse_info(info_item, "◎豆瓣评分")
-                    movie['douban_rating'] = info_item
-                elif info_item.startswith("◎片　　长"):
-                    info_item = parse_info(info_item, "◎片　　长")
-                    movie['duration'] = info_item
-                elif info_item.startswith("◎导　　演"):
-                    info_item = parse_info(info_item, "◎导　　演")
-                    movie['director'] = info_item
+        movie=getMovieDetails(text)
+        print(movie)
+        print("---------------------------------------------")
         movies.append(movie)
     return movies
 
 #获取1到2页，不包含第2页的所有电影详情url，获取7页的时候有bug，还没处理
-result_urls=getUrls(1,2)
+result_urls=getUrls(1,7)
 movies=getContents(result_urls)
 print(movies)
-
